d19b0e8e93b24244d7369a457c8cabcbd21530bf
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-10-10 22:14:32 +0200

    decisionTree

---
+++ R-Course-HTML-Notes/R-for-Data-Science-and-Machine-Learning/Machine Learning with R/tree.R
@@ -0,0 +1,11 @@
+## Decision tree
+library(rpart)
+install.packages('rpart.plot')
+library(rpart.plot)
+
+str(kyphosis)
+tree <- rpart(Kyphosis ~ .,method = 'class', data = kyphosis)
+printcp(tree)
+plot(tree, uniform = T, main = 'Kyphosis Tree')
+text(tree, use.n = T, all = T)
+prp(tree)
